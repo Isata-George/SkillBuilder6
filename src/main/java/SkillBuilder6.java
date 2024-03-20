85256438a973a8d654c9f4f4a8cd6dbc16b0f8fa
IsataGeorge <156387963+IsataGeorge@users.noreply.github.com>
2024-03-20 18:31:20 -0400

    Completed

--- src/main/java/SkillBuilder6.java
+++ src/main/java/SkillBuilder6.java
@@ -9,10 +9,33 @@
 
 public class SkillBuilder6 {
 
-    // replace this line with your constants
+
+    private static final int startState = 0;
+    private static final int isT = 1;
+    private static final int isY = 2;
+
+    private static final String findT = "tT";
+    private static final String findY = "yY";
 
     public static String findTYPattern(String s)
     {
-        // replace this line with your code
+        int currentState = startState;
+        int first = 0;
+        int last = 0;
+        for(int index = 0; index < s.length() - 1; index++){
+            char ch = s.charAt(index);
+            if(findT.indexOf(ch) > -1){
+                first = index;
+                currentState = isT;
+                first = index;
+            }
+            if(currentState == isT){
+                if(findY.indexOf(ch) > findT.indexOf(ch)){
+                    last = index;
+                    currentState = isY;
+                }
+            }
+        }
+        return s.substring(first, last);
     }
 }
